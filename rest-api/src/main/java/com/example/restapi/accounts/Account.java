1a6518acb31a88355cb6c6dcea05faa58b6b3acc
yeonnex <yeonnex@gmail.com>
2022-06-29 10:15:46 +0900

    Add Account domain

---
+++ rest-api/src/main/java/com/example/restapi/accounts/Account.java
@@ -0,0 +1,26 @@
+package com.example.restapi.accounts;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<AccountRole> roles;
+}
---
+++ rest-api/src/main/java/com/example/restapi/accounts/AccountRole.java
@@ -0,0 +1,4 @@
+package com.example.restapi.accounts;
+
+public enum AccountRole {
+}

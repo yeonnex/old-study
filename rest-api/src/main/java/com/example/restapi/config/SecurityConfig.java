874300b28c8ee3fdf3029834e4c7e5b136c9ec7f
yeonnex <yeonnex@gmail.com>
2022-06-29 14:26:11 +0900

    GET /api/** permit all

--- rest-api/src/main/java/com/example/restapi/config/SecurityConfig.java
+++ rest-api/src/main/java/com/example/restapi/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,10 +21,18 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
         http
                 .authorizeRequests()
+                // 정적 리소스 요청 - permit all
                 .antMatchers("/docs/index.html", "/css/**", "/js/**", "/img/**").permitAll()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+
+                // GET 방식의 api 요청 = permit all
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+
+                .and()
+                .formLogin()
+        ;
 
         return http.build();
     }

7566e3031a6d37469cf0f83a4b881aa4b9607761
yeonnex <yeonnex@gmail.com>
2022-07-05 21:30:28 +0900

    intro

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.example.restfulwebservice;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class HelloWorldBean {
+    private final String message;
+}
---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "hello world";
+    }
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("hello world!");
+    }
+    @GetMapping("/hello-world/{name}")
+    public HelloWorldBean helloWorldName(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello, %s",name));
+    }
+
+
+
+}

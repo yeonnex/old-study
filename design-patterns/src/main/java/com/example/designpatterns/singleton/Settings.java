1dec0a4b0503ee529ffc035dc1d2c5ac7299e354
yeonnex <yeonnex@gmail.com>
2022-07-20 15:41:32 +0900

    싱글톤 패턴 - 멀티 스레드에서 구현하는 법 3 - static inner 클래스 사용하기

--- design-patterns/src/main/java/com/example/designpatterns/singleton/Settings.java
+++ design-patterns/src/main/java/com/example/designpatterns/singleton/Settings.java
@@ -2,11 +2,13 @@ package com.example.designpatterns.singleton;
 
 
 public class Settings {
-    private static final Settings INSTANCE = new Settings();
     private Settings(){}
+    private static class SettingHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
 
 
   public static Settings getInstance() {
-        return INSTANCE;
+        return SettingHolder.INSTANCE;
     }
 }

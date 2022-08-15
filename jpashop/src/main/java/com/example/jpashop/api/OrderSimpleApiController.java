155bfb8aa38c93f279977d0b2d35aa3e1c686dca
yeonnex <yeonnex@gmail.com>
2022-08-15 09:25:35 +0900

    간단한 주문 조회 V2 - 엔티티를 DTO 로 변환

--- jpashop/src/main/java/com/example/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/com/example/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package com.example.jpashop.api;
 
+import com.example.jpashop.domain.Address;
 import com.example.jpashop.domain.Order;
+import com.example.jpashop.domain.OrderStatus;
 import com.example.jpashop.repository.OrderRepository;
-import com.example.jpashop.service.OrderService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne (ManyToOne, OneToOne 성능 최적화) <br/>
@@ -19,6 +25,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orders() {
@@ -27,5 +34,31 @@ public class OrderSimpleApiController {
             order.getMember().getName(); // LAZY 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> simpleOrderDto() {
+        List<Order> all = orderRepository.findAll();
+        return all.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Getter
+    @Setter
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order o) {
+            orderId = o.getId();
+            name = o.getMember().getName();
+            orderDate = o.getOrderDate();
+            orderStatus = o.getStatus();
+            address = o.getDelivery().getAddress();
+        }
     }
 }

8b28fa742e815e6e9f9a1261714d4cbbd0b44660
yeonnex <yeonnex@gmail.com>
2022-11-18 14:02:11 +0900

    init querydsl example

---
+++ querydsl-example/src/test/java/com/example/querydslexample/QuerydslExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydslexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

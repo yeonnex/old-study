44ed2cda8d199418e2cde379de05457c4b7c23d7
yeonnex <yeonnex@gmail.com>
2022-11-23 18:05:42 +0900

    Return DTO in queryDsl

--- querydsl-example/src/test/java/com/example/querydslexample/QuerydslBasicTest.java
+++ querydsl-example/src/test/java/com/example/querydslexample/QuerydslBasicTest.java
@@ -1,7 +1,10 @@
 package com.example.querydslexample;
 
+import com.example.querydslexample.dto.MemberDto;
+import com.example.querydslexample.dto.UserDto;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +20,8 @@ import java.util.List;
 
 import static com.example.querydslexample.QMember.member;
 import static com.example.querydslexample.QTeam.team;
+import static com.querydsl.core.types.Projections.bean;
+import static com.querydsl.core.types.Projections.fields;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -292,6 +297,58 @@ public class QuerydslBasicTest {
         }
     }
 
+    /**
+     * 순수 JPA 에서 DTO 조회
+     * - 순수 JPA 에서 DTO 를 조회할 때는 new 명령어를 사용해야함 <br><br/>
+     * - DTO 의 패키지 이름을 다 적어줘야해서 지저분함 <br><br/>
+     * - 생성자 방식만 지원함 <br><br/>
+     */
+    @Test
+    void findDtoByJPQL() {
+        List<MemberDto> resultList = em.createQuery("select new com.example.querydslexample.dto.MemberDto(m.memberName, m.age) from Member m", MemberDto.class)
+                .getResultList();
+
+        assertThat(resultList.get(0)).isInstanceOf(MemberDto.class);
+    }
+
+    /**
+     * QueryDSL 을 사용하여 DTO 조회하기
+     * - 프로퍼티 접근
+     * - 필드 직접 접근
+     */
+    @Test
+    void findDtoByQueryDslSetter() {
+        List<MemberDto> result = queryFactory
+                .select(bean(MemberDto.class, member.memberName, member.age)) // setter 를 통해 값 삽입
+                .from(member)
+                .fetch();
+
+        assertThat(result.get(0)).isInstanceOf(MemberDto.class);
+    }
+
+    @Test
+    void findDtoByQueryDslField() {
+        List<MemberDto> result = queryFactory
+                .select(fields(MemberDto.class, member.memberName, member.age)) // 필드에 직접 값 삽입
+                .from(member)
+                .fetch();
+
+        assertThat(result.get(0)).isInstanceOf(MemberDto.class);
+    }
+
+    @Test
+    void findUserDtoByQueryDsl() {
+        List<UserDto> result = queryFactory
+                .select(fields(UserDto.class, member.memberName.as("name"), member.age))
+                .from(member)
+                .fetch();
+
+        assertThat(result.get(0)).isInstanceOf(UserDto.class);
+        for (UserDto userDto : result) {
+            System.out.println(userDto);
+        }
+
+    }
 
 
 }

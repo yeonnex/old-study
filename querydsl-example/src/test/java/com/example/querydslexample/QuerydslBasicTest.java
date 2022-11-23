88f4ee0f32326940cc656b98f6e0ed3d185fc22f
yeonnex <yeonnex@gmail.com>
2022-11-23 19:03:34 +0900

    동적쿼리 - BooleanBuilder 사용

--- querydsl-example/src/test/java/com/example/querydslexample/QuerydslBasicTest.java
+++ querydsl-example/src/test/java/com/example/querydslexample/QuerydslBasicTest.java
@@ -3,9 +3,9 @@ package com.example.querydslexample;
 import com.example.querydslexample.dto.MemberDto;
 import com.example.querydslexample.dto.QMemberDto;
 import com.example.querydslexample.dto.UserDto;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -358,6 +358,32 @@ public class QuerydslBasicTest {
                 .fetch();
 
         assertThat(result.get(0)).isInstanceOf(MemberDto.class);
+    }
+
+    @Test
+    void dynamicQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.memberName.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+
     }
 
 
